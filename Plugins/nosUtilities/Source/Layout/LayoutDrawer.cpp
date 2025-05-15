48d38f562ae2e05063a695bd9a066792f271945b
Caner <caner.coskun@nodos.dev>
2025-05-15 12:24:14 +0300

    Get drawer & quad layout working

--- Plugins/nosUtilities/Source/Layout/LayoutDrawer.cpp
+++ Plugins/nosUtilities/Source/Layout/LayoutDrawer.cpp
@@ -1,18 +1,114 @@
 // Copyright MediaZ Teknoloji A.S. All Rights Reserved.
 
 #include <Nodos/PluginHelpers.hpp>
-#include <nosVulkanSubsystem/nosVulkanSubsystem.h>
-
+#include <nosVulkanSubsystem/Helpers.hpp>
+#include <glm/glm.hpp>
 #include "Layout_generated.h"
 
 NOS_REGISTER_NAME(LayoutDrawer)
 NOS_REGISTER_NAME(TexturedQuad_Pass)
 NOS_REGISTER_NAME(TexturedQuad_Frag)
 NOS_REGISTER_NAME(TexturedQuad_Vert)
+NOS_REGISTER_NAME(Output)
 namespace nos::utilities
 {
+	inline nosResourceShareInfo DeserializeTextureInfo(sys::vulkan::Texture const& tex)
+	{
+		auto ext = tex.external_memory();
+		return nosResourceShareInfo{
+			.Memory =
+				nosMemoryInfo{
+					.Handle = tex.handle(),
+					.Size = tex.size_in_bytes(),
+					.ExternalMemory = {
+						.HandleType = ext ? static_cast<nosExternalMemoryHandleType>(ext->handle_type()) : NOS_EXTERNAL_MEMORY_HANDLE_TYPE_NONE,
+						.Handle = ext ? ext->handle() : 0,
+						.Offset = tex.offset(),
+						.AllocationSize = ext ? ext->allocation_size() : 0,
+						.PID = ext ? ext->pid() : 0
+					}
+				},
+			.Info =
+				nosResourceInfo{
+					.Type = NOS_RESOURCE_TYPE_TEXTURE,
+					.Texture =
+						nosTextureInfo{
+							.Width = tex.width(),
+							.Height = tex.height(),
+							.Format = (nosFormat)tex.format(),
+							.Filter = (nosTextureFilter)tex.filtering(),
+							.Usage = (nosImageUsage)tex.usage(),
+							.FieldType = (nosTextureFieldType)tex.field_type(),
+						},
+				},
+		};
+	}
+
 	nosResult NOSAPI_CALL ExecuteLayoutDrawer(void* _, nosNodeExecuteParams* params)
 	{
+		nos::NodeExecuteParams args(params);
+		auto const& layout = *args.GetPinData<layout::LayoutDrawList>(NOS_NAME("LayoutDrawList"));
+		if (!layout.items())
+			return NOS_RESULT_SUCCESS;
+		
+		auto outTex = vkss::DeserializeTextureInfo(args[NSN_Output].Data->Data);
+		auto outSize = *args.GetPinData<nos::fb::vec2u>(NOS_NAME("OutputSize"));
+
+		if (outTex.Info.Texture.Width != outSize.x() ||
+			outTex.Info.Texture.Height != outSize.y())
+		{
+			outTex.Memory = {};
+			outTex.Info.Texture.Width = outSize.x();
+			outTex.Info.Texture.Height = outSize.y();
+			nosEngine.SetPinValueByName(args.NodeId, NSN_Output, nos::Buffer::From(vkss::ConvertTextureInfo(outTex)));
+			outTex = vkss::DeserializeTextureInfo(args[NSN_Output].Data->Data);
+		}
+
+		auto& inTexturesFb = *args.GetPinData<flatbuffers::Vector<flatbuffers::Offset<nos::sys::vulkan::Texture>>>(NOS_NAME("InputTextures"));
+		std::vector<nosResourceShareInfo> inTextures;
+		inTextures.reserve(inTexturesFb.size());
+		for (auto* texture : inTexturesFb)
+			inTextures.push_back(DeserializeTextureInfo(*texture));
+
+
+		std::vector<nosRunPassParams> passes;
+		bool first = true;
+		auto cmd = vkss::BeginCmd(NOS_NAME("TextureCombiner"), args.NodeId);
+		for (auto* item : *layout.items())
+		{
+			if (item->texture_id() >= inTextures.size())
+			{
+				nosEngine.LogE("Invalid texture id: %d", item->texture_id());
+				continue;
+			}
+			auto const& inputTex = inTextures[item->texture_id()];
+			auto pos = item->position();
+			auto size = item->size();
+
+			std::array bindings = { vkss::ShaderBinding(NOS_NAME("Offset"), pos),
+								   vkss::ShaderBinding(NOS_NAME("Size"), size),
+								   vkss::ShaderBinding(NOS_NAME("Input"), inputTex) };
+
+			nosVertexData vertexData = {
+				.DepthFunc = NOS_DEPTH_FUNCTION_ALWAYS,
+				.DepthWrite = NOS_FALSE,
+				.DepthTest = NOS_FALSE,
+			};
+			nosRunPassParams runPassParams{
+				.Key = NSN_TexturedQuad_Pass,
+				.Bindings = bindings.data(),
+				.BindingCount = bindings.size(),
+				.Output = outTex,
+				.Vertices = vertexData,
+				.Wireframe = NOS_FALSE,
+				.Benchmark = NOS_FALSE,
+				.DoNotClear = !first,
+				.ClearCol = {0.0f, 0.0f, 0.0f, 1.0f}
+			};
+			nosVulkan->RunPass(cmd, &runPassParams);
+			first = false;
+		}
+		vkss::EndCmd(cmd, false, nullptr);
 		return NOS_RESULT_SUCCESS;
 	}
 	nosResult RegisterLayoutDrawer(nosNodeFunctions* fn)
--- Plugins/nosUtilities/Source/Layout/LayoutNodes.cpp
+++ Plugins/nosUtilities/Source/Layout/LayoutNodes.cpp
@@ -11,22 +11,22 @@ namespace nos::utilities
 	{
 		nos::NodeExecuteParams args(params);
 		auto& outSize = *args.GetPinData<nos::fb::vec2u>(NOS_NAME("OutputSize"));
-		auto items = args.GetPinData<flatbuffers::Vector<flatbuffers::Offset<layout::FreeLayoutItem>>>(NOS_NAME("Items"));
+		auto items = args.GetPinData<flatbuffers::Vector<const layout::FreeLayoutItem*>>(NOS_NAME("Items"));
 
 		layout::TLayoutDrawList drawList{};
 		drawList.items.reserve(items->size());
 
-		uint32_t textureId = 0;
-		for (auto* item : *items)
+		int textureId = 0;
+		for (auto itemPtr : *items)
 		{
+			auto& item = *itemPtr;
 			layout::LayoutTexturedQuadDrawItem drawItem{};
-			drawItem.mutable_position() = nos::fb::vec2(item->position().x() / (float) outSize.x(), item->position().y() / (float) outSize.y());
-			drawItem.mutable_size() = nos::fb::vec2(item->size().x() / (float)outSize.x(), item->size().y() / (float)outSize.y());
+			drawItem.mutable_position() = nos::fb::vec2(item.position().x() / (float) outSize.x(), item.position().y() / (float) outSize.y());
+			drawItem.mutable_size() = nos::fb::vec2(item.size().x() / (float)outSize.x(), item.size().y() / (float)outSize.y());
 			drawItem.mutate_texture_id(textureId);
 			drawList.items.push_back(std::move(drawItem));
 			textureId++;
 		}
-
 		nosEngine.SetPinValueByName(args.NodeId, NOS_NAME("OutLayout"), nos::Buffer::From(drawList));
 		return NOS_RESULT_SUCCESS;
 	}
@@ -38,6 +38,20 @@ namespace nos::utilities
 	}
 	nosResult NOSAPI_CALL ExecuteQuadLayout(void* _, nosNodeExecuteParams* params)
 	{
+		nos::NodeExecuteParams args(params);
+		layout::TLayoutDrawList drawList{};
+		drawList.items.reserve(4);
+		for (size_t i = 0; i < 4; i++)
+		{
+			float x = 0.0f + (i % 2) * 0.5f;
+			float y = 0.0f + (i / 2) * 0.5f;
+			layout::LayoutTexturedQuadDrawItem drawItem{};
+			drawItem.mutable_position() = nos::fb::vec2(x, y);
+			drawItem.mutable_size() = nos::fb::vec2(0.5f, 0.5f);
+			drawItem.mutate_texture_id(i);
+			drawList.items.push_back(std::move(drawItem));
+		}
+		nosEngine.SetPinValueByName(args.NodeId, NOS_NAME("OutLayout"), nos::Buffer::From(drawList));
 		return NOS_RESULT_SUCCESS;
 	}
 	nosResult RegisterQuadLayout(nosNodeFunctions* fn)
