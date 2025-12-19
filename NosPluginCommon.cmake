05a38322851887039306de3da186037586b8c52f
Caner <caner.coskun@nodos.dev>
2025-12-19 13:22:22 +0300

    Convert shader compilation to execute on build step as before & update nosPythonDeps

--- NosPluginCommon.cmake
+++ NosPluginCommon.cmake
@@ -5,14 +5,17 @@ function(nos_plugin_common current_dir out_target_dependencies out_target_defini
 	nos_get_module("nos.sys.shaderc" "2.1" NOS_SHADERC_TARGET)
 	nos_find_module_path("nos.sys.shaderc" "2.1" NOS_SHADERC_PATH)
 
-    execute_process(
+    set(compile_shaders_target_name "modules_shader_compilation")
+    add_custom_target(${compile_shaders_target_name}
+        ALL
         COMMAND ${Python3_EXECUTABLE} ${NOS_SHADERC_PATH}/Scripts/compile_shaders.py
-        RESULT_VARIABLE COMPILE_SHADERS_RESULT
         WORKING_DIRECTORY ${current_dir}
+        COMMENT "Compiling shaders..."
+        GLOBAL
     )
-    if (NOT ${COMPILE_SHADERS_RESULT} EQUAL "0")
-        message(FATAL_ERROR "Failed to compile shaders. Process returned ${COMPILE_SHADERS_RESULT}.")
-    endif()
+
+    set(${out_target_dependencies} "${compile_shaders_target_name}" PARENT_SCOPE)
 
     set(${out_target_definitions} "NOS_DISABLE_DEPRECATED" PARENT_SCOPE)
-endfunction()
+    nos_group_targets("${compile_shaders_target_name}" "Build Tasks")
+    endfunction()
