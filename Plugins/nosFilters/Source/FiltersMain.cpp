33b590a579d6886e0eba516fc96a50ccbe348328
atkurtul <atilkurtulmus@gmail.com>
2026-02-16 11:28:30 +0300

    add migration functions

--- Plugins/nosFilters/Source/FiltersMain.cpp
+++ Plugins/nosFilters/Source/FiltersMain.cpp
@@ -23,6 +23,29 @@ enum Filters : int
 	Count
 };
 
+void GetRenamedNodeClasses(nosName* outRenamedFrom, nosName* outRenamedTo, size_t* outSize)
+{
+    static std::vector<std::pair<nos::Name, nos::Name>> renames = {
+        {NOS_NAME("zd.utilities.3WayColorCorrect"), NOS_NAME("nos.filters.3WayColorCorrect")},
+        {NOS_NAME("zd.utilities.ColorMatrix"), NOS_NAME("nos.filters.ColorMatrix")},
+        {NOS_NAME("zd.utilities.Crop"), NOS_NAME("nos.filters.Crop")},
+        {NOS_NAME("zd.utilities.Premultiply"), NOS_NAME("nos.filters.Premultiply")},
+        {NOS_NAME("zd.utilities.Unpremultiply"), NOS_NAME("nos.filters.Unpremultiply")},
+    };
+
+    if (!outRenamedFrom)
+    {
+        *outSize = renames.size();
+        return;
+    }
+
+    for (size_t i = 0; i < renames.size(); ++i)
+    {
+        outRenamedFrom[i] = renames[i].first;
+        outRenamedTo[i] = renames[i].second;
+    }
+}
+
 NOSAPI_ATTR nosResult NOSAPI_CALL ExportNodeFunctions(size_t* outSize, nosNodeFunctions** outList)
 {
 	if (!outList)
@@ -35,6 +58,7 @@ extern "C"
 NOSAPI_ATTR nosResult NOSAPI_CALL nosExportPlugin(nosPluginFunctions* outFunctions)
 {
 	outFunctions->ExportNodeFunctions = ExportNodeFunctions;
+    outFunctions->GetRenamedNodeClasses = GetRenamedNodeClasses;
 	return NOS_RESULT_SUCCESS;
 }
 }
