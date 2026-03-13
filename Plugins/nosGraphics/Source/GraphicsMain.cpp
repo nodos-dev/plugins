7a7d9c9247819bf436446cc44c779ad634ec7d70
Caner Coskun <caner.coskun@nodos.dev>
2026-03-13 12:48:43 +0300

    Add option to render on top of existing depth for BillboardMask & add NormalizedDepthToDepthBuffer node for depth conversion (#54)

--- Plugins/nosGraphics/Source/GraphicsMain.cpp
+++ Plugins/nosGraphics/Source/GraphicsMain.cpp
@@ -18,12 +18,14 @@ enum Nodes : int
 	TrackToView,
 	BillboardMask,
 	BillboardPositions,
+	NormalizedDepthToDepthBuffer,
 	Count
 };
 
 nosResult RegisterTrackToView(nosNodeFunctions*);
 nosResult RegisterBillboardMask(nosNodeFunctions*);
 nosResult RegisterBillboardPositions(nosNodeFunctions*);
+nosResult RegisterNormalizedDepthToDepthBuffer(nosNodeFunctions*);
 
 struct PluginFunctions : nos::PluginFunctions
 {
@@ -47,6 +49,7 @@ struct PluginFunctions : nos::PluginFunctions
 				GEN_CASE_NODE(TrackToView)
 				GEN_CASE_NODE(BillboardMask)
 				GEN_CASE_NODE(BillboardPositions)
+				GEN_CASE_NODE(NormalizedDepthToDepthBuffer)
 			default: break;
 			}
 		}
---
+++ Plugins/nosGraphics/Source/NormalizedDepthToDepthBuffer.cpp
@@ -0,0 +1,119 @@
+// Copyright MediaZ Teknoloji A.S. All Rights Reserved.
+
+#include <Nodos/PluginHelpers.hpp>
+#include <Graphics_generated.h>
+#include <nosVulkanSubsystem/Helpers.hpp>
+#include <glm/glm.hpp>
+
+namespace nos::graphics
+{
+NOS_REGISTER_NAME(NormalizedDepthToDepthBuffer)
+
+NOS_REGISTER_NAME(InDepth)
+NOS_REGISTER_NAME(RenderView)
+NOS_REGISTER_NAME(Scale)
+NOS_REGISTER_NAME(OutDepth)
+
+NOS_REGISTER_NAME(NormalizedDepthToDepthBuffer_Pass)
+NOS_REGISTER_NAME(NormalizedDepthToDepthBuffer_Frag)
+
+struct NormalizedDepthToDepthBuffer : NodeContext
+{
+	std::optional<vkss::Resource> TempColorOutput;
+
+	using NodeContext::NodeContext;
+
+	nosResult ExecuteNode(nosNodeExecuteParams* params) override
+	{
+		NodeExecuteParams pins(params);
+
+		auto inputTexture = vkss::DeserializeTextureInfo(pins[NSN_InDepth].Data->Data);
+		TRenderView view = pins.GetPinData<TRenderView>(NSN_RenderView);
+		if (!view.projection)
+		{
+			nosEngine.LogW("NormalizedDepthToDepthBuffer requires a valid RenderView projection.");
+			return NOS_RESULT_FAILED;
+		}
+
+		float scale = *pins.GetPinData<float>(NSN_Scale);
+		glm::vec2 clipPlanes = reinterpret_cast<glm::vec2 const&>(view.projection->clip_planes);
+
+		nosResourceShareInfo outputDepth = vkss::DeserializeTextureInfo(pins[NSN_OutDepth].Data->Data);
+		if (!TempColorOutput || TempColorOutput->Info.Texture.Width != outputDepth.Info.Texture.Width ||
+			TempColorOutput->Info.Texture.Height != outputDepth.Info.Texture.Height)
+			TempColorOutput = vkss::Resource::Create(
+				nosTextureInfo{.Width = outputDepth.Info.Texture.Width,
+							   .Height = outputDepth.Info.Texture.Height,
+							   .Format = NOS_FORMAT_R8_UNORM,
+							   .Filter = NOS_TEXTURE_FILTER_LINEAR,
+							   .Usage = NOS_IMAGE_USAGE_RENDER_TARGET},
+				"NormalizedDepthToDepthBuffer Temp Output");
+		if (!TempColorOutput)
+		{
+			nosEngine.LogE("Failed to create temporary render target for NormalizedDepthToDepthBuffer.");
+			return NOS_RESULT_FAILED;
+		}
+
+		std::vector<nosShaderBinding> bindings;
+		bindings.emplace_back(vkss::ShaderBinding(NOS_NAME("Input"), inputTexture));
+		bindings.emplace_back(vkss::ShaderBinding(NSN_Scale, scale));
+		bindings.emplace_back(vkss::ShaderBinding(NOS_NAME("ClipNear"), clipPlanes.x));
+		bindings.emplace_back(vkss::ShaderBinding(NOS_NAME("ClipFar"), clipPlanes.y));
+
+		nosCmd cmd = {};
+		nosCmdBeginParams beginParams{.Name = NOS_NAME("Normalized Depth To Depth Buffer"),
+									  .AssociatedNodeId = NodeId,
+									  .OutCmdHandle = &cmd};
+		nosVulkan->Begin(&beginParams);
+
+		nosRunPassParams pass = {};
+		pass.Key = NSN_NormalizedDepthToDepthBuffer_Pass;
+		pass.Bindings = bindings.data();
+		pass.BindingCount = bindings.size();
+		pass.Output = *TempColorOutput;
+		pass.Benchmark = 0;
+		pass.DoNotClear = false;
+		pass.DepthAttachment = {
+			.DepthBuffer = outputDepth,
+			.DoNotClear = false,
+			.ClearValue = 1.0f,
+		};
+		pass.Vertices = {
+			.IndexCount = 6,
+			.DepthFunc = NOS_DEPTH_FUNCTION_ALWAYS,
+			.DepthWrite = true,
+			.DepthTest = true,
+		};
+
+		nosVulkan->RunPass(cmd, &pass);
+		nosVulkan->End(cmd, nullptr);
+		return NOS_RESULT_SUCCESS;
+	}
+};
+
+nosResult RegisterNormalizedDepthToDepthBuffer(nosNodeFunctions* fn)
+{
+	NOS_BIND_NODE_CLASS(NSN_NormalizedDepthToDepthBuffer, NormalizedDepthToDepthBuffer, fn);
+
+	fs::path root = nosEngine.Module->RootFolderPath;
+	auto fragPath = (root / "Shaders" / "NormalizedDepthToDepthBuffer.frag").generic_string();
+
+	nosShaderInfo shader{
+		.ShaderName = NSN_NormalizedDepthToDepthBuffer_Frag,
+		.Source = {.Stage = NOS_SHADER_STAGE_FRAG, .GLSLPath = fragPath.c_str()},
+		.AssociatedNodeClassName = NSN_NormalizedDepthToDepthBuffer,
+	};
+
+	auto ret = nosVulkan->RegisterShaders(1, &shader);
+	if (ret != NOS_RESULT_SUCCESS)
+		return ret;
+
+	nosPassInfo pass = {
+		.Key = NSN_NormalizedDepthToDepthBuffer_Pass,
+		.Shader = NSN_NormalizedDepthToDepthBuffer_Frag,
+		.MultiSample = 1,
+	};
+
+	return nosVulkan->RegisterPasses(1, &pass);
+}
+} // namespace nos::graphics
